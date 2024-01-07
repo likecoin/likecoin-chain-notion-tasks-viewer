d03fdc4ac7ae6de0c9491e54df98966ad18906e9
Ng Wing Tat, David <i@ngwingt.at>
2024-01-07 23:07:44 +0800

    🎉 Init with Nuxt 3 & Nuxt UI v2.11.1

---
+++ app.vue
@@ -0,0 +1,39 @@
+<template>
+  <UContainer class="py-4">
+    <UCard :ui="{ body: { padding: '' } }">
+      <template #header>
+        <h1 class="text-xl font-bold">
+          LikeCoin Chain Notion Tasks Viewer<br />
+          <span class="text-[0.75em] text-slate-400">by Liker Land Team</span>
+        </h1>
+      </template>
+
+      <UTable :columns="columns" :rows="[]" />
+    </UCard>
+  </UContainer>
+</template>
+
+<script setup>
+const tasksColumns = [
+  {
+    key: "date",
+    label: "Date",
+  },
+  {
+    key: "name",
+    label: "Task Name",
+  },
+  {
+    key: "priority",
+    label: "Task Priority",
+  },
+  {
+    key: "assignees",
+    label: "Assignees",
+  },
+  {
+    key: "mandays",
+    label: "Man-days",
+  },
+];
+</script>
